483f64e738b2030f4c206b97cddaece33dad48cf
subikstha <subsatch@gmail.com>
2024-06-14 14:02:34 +0545

    sign up and sign in form actions completed

---
+++ actions/auth.ts
@@ -0,0 +1,48 @@
+'use server'
+import { cookies } from 'next/headers'
+import { signin, signup } from '@/utils/authTools'
+import { z } from 'zod'
+import { redirect } from 'next/navigation'
+// This is the name of the cookie that we will be setting
+import { COOKIE_NAME } from '@/utils/constants'
+
+const authSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+})
+
+export const registerUser = async (prevState: any, formData: FormData) => {
+  const data = authSchema.parse({
+    email: formData.get('email'),
+    password: formData.get('password'),
+  })
+
+  try {
+    const { token } = await signup(data)
+    cookies().set(COOKIE_NAME, token)
+  } catch (e) {
+    console.error(e)
+    return { message: 'Failed to sign you up' }
+  }
+
+  //  We put the redirect after the try catch block, because if we put it in the try block, then
+  // it will catch an error
+  redirect('/dashboard')
+}
+
+export const signinUser = async (prevState: any, formData: FormData) => {
+  const data = authSchema.parse({
+    email: formData.get('email'),
+    password: formData.get('password'),
+  })
+
+  try {
+    const { token } = await signin(data)
+    cookies().set(COOKIE_NAME, token)
+  } catch (e) {
+    console.error(e)
+    return { message: 'Failed to sign you in' }
+  }
+
+  redirect('/dashboard')
+}
--- components/SigninForm.tsx
+++ components/SigninForm.tsx
@@ -1,10 +1,22 @@
 'use client'
 import { Input } from '@nextui-org/react'
 import Link from 'next/link'
+import Submit from './SubmitButton'
+import { useFormState } from 'react-dom'
+import { signinUser } from '@/actions/auth'
+
+const initState = { message: null }
 
 const SigninForm = () => {
+  const [formState, action] = useFormState<{ message: string | null }>(
+    signinUser,
+    initState
+  )
   return (
-    <form className="bg-content1 border border-default-100 shadow-lg rounded-md p-3 flex flex-col gap-2 ">
+    <form
+      action={action}
+      className="bg-content1 border border-default-100 shadow-lg rounded-md p-3 flex flex-col gap-2 "
+    >
       <h3 className="my-4">Sign in</h3>
       <Input
         fullWidth
@@ -22,10 +34,12 @@ const SigninForm = () => {
         type="password"
         placeholder="Password"
       />
+      <Submit label={'Sign In'} />
 
       <div>
         <Link href="/signup">{`Don't have an account?`}</Link>
       </div>
+      {formState?.message && <p>{formState.message}</p>}
     </form>
   )
 }
